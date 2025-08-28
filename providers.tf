e2794c416c39f9f76927267aacc1dd2604cd143b
Vyacheslav Butyrkin <slava.butyrkin@gmail.com>
2025-08-28 21:27:12 +0200

    No private data in providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,6 @@
 provider "yandex" {
-  service_account_key_file = "C:/otus/scripts/new_authorized_key.json"
-
-  cloud_id  = "b1gq6b8c2o97qii09u16"
-  folder_id = "b1gi4qo3j468n0j3bkhv"
-  zone      = "ru-central1-d"
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+  service_account_key_file = var.sa_key_file
 }
